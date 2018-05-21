75123288899444ebcf78b2da9323a862edcfa0f4
tamasPetki <tonertop@gmail.com>
2018-05-21 21:51:15 +0200

    init

---
+++ methods.java
@@ -0,0 +1,11 @@
+class methods {
+    private int[][] multi = new int[][]{
+            { 2, 0, 4, 1241, 12, 5, 11, 1110, -42, 424, 1, 12323 },
+            { 1, 3, 5, 7 },
+            { 321, 320, 32, 3, 41241, -11, -12, -13, -66, -688 }
+    };
+
+    public int[] nearby (int x, int y, int range) {
+
+    }
+}
